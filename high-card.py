9c0a1824a655614ed3e78cba0c67b4089e34284b
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2020-12-09 07:03:56 +0200

    Update high-card.py

--- high-card.py
+++ high-card.py
@@ -16,11 +16,11 @@ deck=[[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13]]
 
 
 
-def sigmoid(x):
-    """
-    Функция активации
-    """
-    return 1 / (1 + 2.718**(-x))
+def sigmoid(t):
+    return 1/(1+np.exp(-t))
+
+def sigmoid_derivative(p):
+    return p * (1 - p)
 
 
 '''
@@ -34,15 +34,20 @@ card = [0,0,0,0,0, 0,0,0,0,0, 0,0,1]
 
 X = card
 
-import numpy
+import numpy as np
 
 class NeuralNetwork:
     def __init__(self, X, y):
         self.input = X
+        self.w1 = np.random.rand(len(X), 2)
+        self.w2 = np.random.rand(2, 1)
+        self.y = y
+        self.output = np.zeros(y.shape)
 
 
-    def feedforward(self, L1, L2, W):
-        '''
+
+    def feedforward(self):
+        """
         Функция для прямого прохождения между двумя слоями нейронов
         L1 входящий слой
         L2 выходящий
@@ -54,22 +59,45 @@ class NeuralNetwork:
         2.2 Вложенный цикл по входным нейронам. 
         Умножать их значения на веса соответствующих связей и складывать
         Результат пропустить через функцию активации, получая новое значение выходного нейрона
-        '''
-        self.L1 = 
+        """
+        self.layer1 = sigmoid(np.dot(self.input, self.w1))
+        self.layer2 = sigmoid(np.dot(self.layer1, self.w2))
+        return self.layer2
 
 
+    def backprop(self):
+        """
+        """
+        print(f"{self.layer1.T=}")
+        print(f"{2*(self.y -self.output)*sigmoid_derivative(self.output)=}")
 
-a = numpy.random.rand(1, len(X))
-# print(f"{a=}")
 
-b = numpy.zeros(a.shape)
-# print(f"{b=}")
+        d_w2 = np.dot(self.layer1.T, 
+            2*(self.y -self.output)*sigmoid_derivative(self.output))
+        d_w1 = np.dot(self.input.T, 
+            np.dot(2*(self.y -self.output)*sigmoid_derivative(self.output), self.w2.T)*sigmoid_derivative(self.layer1))
+    
+        self.w1 += d_w1
+        self.w2 += d_w2
+
+    def train(self, X, y):
+        self.output = self.feedforward()
+        self.backprop()
+
+
+X = np.array([0,0,0,0,0, 0,0,0,0,0, 0,0,1], dtype=float)
+y = np.array([1], dtype=float)
 
 NN = NeuralNetwork(X, y)
-# print(f"{NN.w2=}")
+for i in range(1500): # trains the NN 1,000 times
+    pass
+    if i % 500 ==0: 
+        print ("for iteration # " + str(i) + "\n")
+        print ("Input : \n" + str(X))
+        print ("Actual Output: \n" + str(y))
+        print ("Predicted Output: \n" + str(NN.feedforward()))
+        print ("Loss: \n" + str(np.mean(np.square(y - NN.feedforward())))) # mean sum squared loss
+        print ("\n")
+  
+    NN.train(X, y)
 
-d = numpy.dot([[2,1], [1,1]],[[2,2], [1,1]])
-print(f"{d=}")
-
-x='5'
-print(f"{numpy.array(d).T=}")
