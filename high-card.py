97f368b765b13e7f02a16c9d44344567bf1b5fee
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-03-15 22:38:42 +0200

    clean

--- high-card.py
+++ high-card.py
@@ -1,103 +1,111 @@
+'''
+игрок со старшей картой побеждает.
 
-
-deck = ['1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A', '10A', '11A', '12A', '13A', 
-        '1B', '2B', '3B', '4B', '5B', '6B', '7B', '8B', '9B', '10B', '11B', '12B', '13B', 
-        '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', '11C', '12C', '13C', 
-        '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', '11D', '12D', '13D']
-
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-deck=[[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13]]
-
-
-
-def sigmoid(t):
-    return 1/(1+np.exp(-t))
-
-def sigmoid_derivative(p):
-    return p * (1 - p)
-
+в колоде 10 карт, игроки получают по одной
+[1,2,3,4,5,6,7,8,9,10]
 
 '''
-входящий сигнал - активная карта из 13-ти
-два игрока
-побеждает игрок со старшей картой.
-
-'''
-
-card = [0,0,0,0,0, 0,0,0,0,0, 0,0,1]
-
-X = card
 
 import numpy as np
 
-class NeuralNetwork:
-    def __init__(self, X, y):
-        self.input = X
-        self.w1 = np.random.rand(len(X), 2)
-        self.w2 = np.random.rand(2, 1)
-        self.y = y
-        self.output = np.zeros(y.shape)
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+r = 0.1
+
+
+class NeuralNetwork(object):
+    """docstring for NeuralNetwork"""
+    def __init__(self, X, t):
+        self.input = X
+        self.t = t
+        self.w1=np.array([ [0.34740112, 0.71587649, 0.54477065],
+                           [0.12048776, 0.80215174, 0.7848461 ],
+                           [0.6657934 , 0.99960724, 0.53942614],
+                           [0.72586272, 0.6453454 , 0.76229148],
+                           [0.84048451, 0.41317105, 0.5744454 ],
+                           [0.32745197, 0.42230799, 0.85048843],
+                           [0.32768473, 0.83817198, 0.81213001],
+                           [0.29800999, 0.11962701, 0.60332183],
+                           [0.07127859, 0.04723507, 0.58615689],
+                           [0.08824525, 0.77525399, 0.53755418]])
+
+        self.w2=np.array([ [0.95994211, 0.59427341],
+                           [0.27699257, 0.15645139],
+                           [0.05190993, 0.23507123]])
+
+        self.w3=np.array([ [0.59027993, 1.25091387],
+                           [0.65712936, 1.18164082]])
+
+        self.out = None
+        self.E = None
 
 
     def feedforward(self):
-        """
-        Функция для прямого прохождения между двумя слоями нейронов
-        L1 входящий слой
-        L2 выходящий
-        W  связи между ними
-
-        1 По массиву W определить количество входных и выходных нейронов
-
-        2.1 Цикл по выходным нейронам, в начале каждой итерации обнуляеть выходной нейрон
-        2.2 Вложенный цикл по входным нейронам. 
-        Умножать их значения на веса соответствующих связей и складывать
-        Результат пропустить через функцию активации, получая новое значение выходного нейрона
-        """
-        self.layer1 = sigmoid(np.dot(self.input, self.w1))
-        self.layer2 = sigmoid(np.dot(self.layer1, self.w2))
-        return self.layer2
-
+        self.out_L1 = sigmoid(np.dot(self.input, self.w1))
+        self.out_L2 = sigmoid(np.dot(self.out_L1, self.w2))
+        self.out = np.dot(self.out_L2, self.w3)
+        return self.out
 
     def backprop(self):
-        """
-        """
-        print(f"{self.layer1.T=}")
-        print(f"{2*(self.y -self.output)*sigmoid_derivative(self.output)=}")
+        w1 = self.w1
+        w2 = self.w2
+        w3 = self.w3
+        X = self.input
+        out_L1 = self.out_L1
+        out_L2 = self.out_L2
+        out = self.out
+        t = self.t
+
+        E_total = 1/len(out) * np.sum((out - t)**2)
+        E_total = 1/2 * (out[0]-t[0])**2 + 1/2 * (out[1]-t[1])**2
+        self.E = E_total
+        r = 0.3 
+
+        w = w3[0][0]
+        t = self.t[0]
+        out_y = out[0]
+        dw = -(t-out_y) * out_y*(1-out_y) * out_L1[0]
+        w3[0][0] = w - r * dw
+
+        w = w3[0][1]
+        t = self.t[0]
+        out_y = out[0]
+        dw = -(t-out_y) * out_y*(1-out_y) * out_L1[1]
+        w3[0][1] = w - r * dw
+
+        w = w3[1][0]
+        t = self.t[1]
+        out_y = out[1]
+        dw = -(t-out_y) * out_y*(1-out_y) * out_L1[1]
+        w3[1][0] = w - r * dw
+
+        w = w3[1][1]
+        t = self.t[1]
+        out_y = out[1]
+        dw = -(t-out_y) * out_y*(1-out_y) * out_L1[0]
+        w3[1][1] = w - r * dw
+
+        self.w3 = w3
+
+        return
+
+        
 
 
-        d_w2 = np.dot(self.layer1.T, 
-            2*(self.y -self.output)*sigmoid_derivative(self.output))
-        d_w1 = np.dot(self.input.T, 
-            np.dot(2*(self.y -self.output)*sigmoid_derivative(self.output), self.w2.T)*sigmoid_derivative(self.layer1))
-    
-        self.w1 += d_w1
-        self.w2 += d_w2
+X = [0,0,0,0,0,0,0,0,0,1]
+t = [1,9]
+NN = NeuralNetwork(X,t)
+print(f"{NN.feedforward()=}")
+for i in range(1000):
+    if i%200 == 0:
+        print(f"{NN.out=}")
+        print(f"{NN.E=}")
+    NN.feedforward()
+    NN.backprop()
 
-    def train(self, X, y):
-        self.output = self.feedforward()
-        self.backprop()
-
-
-X = np.array([0,0,0,0,0, 0,0,0,0,0, 0,0,1], dtype=float)
-y = np.array([1], dtype=float)
-
-NN = NeuralNetwork(X, y)
-for i in range(1500): # trains the NN 1,000 times
-    pass
-    if i % 500 ==0: 
-        print ("for iteration # " + str(i) + "\n")
-        print ("Input : \n" + str(X))
-        print ("Actual Output: \n" + str(y))
-        print ("Predicted Output: \n" + str(NN.feedforward()))
-        print ("Loss: \n" + str(np.mean(np.square(y - NN.feedforward())))) # mean sum squared loss
-        print ("\n")
-  
-    NN.train(X, y)
+print(f"{NN.w3=}")
 
